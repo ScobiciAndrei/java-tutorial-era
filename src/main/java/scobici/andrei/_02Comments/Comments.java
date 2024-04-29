625ec1587c33b8c4609721b2bbc436eff460722e
Tutorial <scobiciandrei95@gmail.com>
2024-04-29 14:14:30 +0200

    this class show how to add a comments

---
+++ src/main/java/scobici/andrei/_02Comments/Comments.java
@@ -0,0 +1,22 @@
+package scobici.andrei._02Comments;
+
+/**
+ * This is a class for showing how comments are working
+ * press ctrl+Q to check the java doc
+ */
+public class Comments {
+    /***
+     * @param args lista de argumente la startarea programului
+     * Daca metoda are return atunci poti folosi @return pentru a descrie informatia despre return
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I learn java");
+
+        System.out.print("Hello" );
+        System.out.print(" i print i same line ");
+
+        //System.out.println(This line produce errore);
+        System.out.println("this line have a comment"); //this line is good
+    }
+}
