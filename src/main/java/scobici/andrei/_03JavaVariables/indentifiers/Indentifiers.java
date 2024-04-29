e178353611dae94ae960339e921aeb83dc098ef7
Tutorial <scobiciandrei95@gmail.com>
2024-04-29 14:25:52 +0200

    a good practice is to use a name

---
+++ src/main/java/scobici/andrei/_03JavaVariables/indentifiers/Indentifiers.java
@@ -0,0 +1,16 @@
+package scobici.andrei._03JavaVariables.indentifiers;
+
+/**
+ * Clasa data arata cum trebuie sa fie o variabile declarata clar
+ */
+public class Indentifiers {
+    /**
+     * @param args declararea unui variabile in mod clar si explicit
+     */
+    public static void main(String[] args) {
+
+        int minInHour = 60; // good description
+
+        int m = 60; // it's not good because we don't understand what is m value;
+    }
+}
